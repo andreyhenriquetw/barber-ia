f2a3d66c37b1068f215e2e7ba161a6d0724aa281
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2025-11-15 00:45:21 -0300

    feat: add correção de cor and imagem

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
 import Header from "./_components/header";
-import Banner from "../public/banner011.png";
+import Banner from "../public/agendebanner.png";
 
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <main>
+      <div>
+        <Header />
 
-      <Image
-        src={Banner}
-        alt="Agende agora!"
-        sizes="100vw"
-        className="h-auto w-full"
-      />
-      <div className="px-5"></div>
-    </div>
+        {/* CONTAINER RELATIVO */}
+        <div className="relative w-full">
+          {/* IMAGEM */}
+          <Image
+            src={Banner}
+            alt="Agende agora!"
+            sizes="100vw"
+            className="w-full object-cover object-top"
+          />
+
+          {/* BOTÃO NA ESQUERDA EMBAIXO */}
+          <button className="absolute right-6 bottom-5 animate-bounce rounded-lg bg-blue-400 px-4 py-2 font-semibold text-white shadow-md transition hover:shadow-lg hover:brightness-110 active:scale-95">
+            AGENDAR AGORA
+          </button>
+        </div>
+
+        <div className="px-5">
+          <h2>Agendamentos</h2>
+        </div>
+      </div>
+    </main>
   );
 };
 
